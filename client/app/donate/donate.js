9580e5eea5f191827fc10a39baa9d9d1a4f3c78c
gensc004 <gensc004@morris.umn.edu>
2015-12-11 03:11:00 -0600

    removed routing for construction page.

--- client/app/donate/donate.js
+++ client/app/donate/donate.js
@@ -2,10 +2,10 @@
 
 angular.module('resthavenHorseRescueApp')
   .config(function ($stateProvider) {
-    $stateProvider
-      .state('donate', {
-        url: '/donate',
-        templateUrl: 'app/donate/donate.html',
-        controller: 'DonateCtrl'
-      });
+    // $stateProvider
+    //   .state('donate', {
+    //     url: '/donate',
+    //     templateUrl: 'app/donate/donate.html',
+    //     controller: 'DonateCtrl'
+    //   });
   });
